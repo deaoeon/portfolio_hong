1147060d603091ada0cdab771bf55b390b9228e1
deaoeon <deaoeon1@gmail.com>
2026-04-07 00:08:22 +0900

    깃허브 배포3

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App.jsx';
-
-const client = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 60_000,
-            gcTime: 300_000,
-            refetchOnWindowFocus: false,
-            retry: 1,
-        },
-    },
-});
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <QueryClientProvider client={client}>
-            <App />
-            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-        </QueryClientProvider>
-    </StrictMode>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
