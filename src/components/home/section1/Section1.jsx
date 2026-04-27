741c250ece1263abaa6fb9b072b1faceab1872fb
deaoeon <deaoeon1@gmail.com>
2026-04-28 03:47:41 +0900

    프로필사진 추가

--- src/components/home/section1/Section1.jsx
+++ src/components/home/section1/Section1.jsx
@@ -23,7 +23,7 @@ const Section1 = () => {
     </div>
 
     <div className="section1__right">
-      <img src="/portfolio_hong/images/hong2.jpg" alt="profile" />
+      <img src="/portfolio_hong/images/hong1.jpg" alt="profile" />
     </div>
 
   </div>
